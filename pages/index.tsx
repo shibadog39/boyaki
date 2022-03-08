48bffa0a64f85244247f335d06ef3764e4920c90
shibadog39 <shibadog1121@gmail.com>
2022-03-08 19:07:10 +0900

    handle multi environment

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! deployed</a>
+          Welcome to <a href="https://nextjs.org">Next.js! prd</a>
         </h1>
 
         <p className={styles.description}>
